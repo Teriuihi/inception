db35a0406a54445dace1a95da2f315624ba80744
Teri <teriuihi@alttd.com>
2023-05-04 20:50:11 +0200

    changes mariadb works

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,14 +4,14 @@
 
 service mysql start 
 
-
-echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
-
-mysql < db1.sql
+mysql -e "CREATE DATABASE IF NOT EXISTS $db1_name ;"
+mysql -e "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;"
+mysql -e "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;"
+mysql -e "CREATE DATABASE IF NOT EXISTS $db_name ;"
+mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;"
+mysql -e "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '' ;"
+mysql -e "FLUSH PRIVILEGES;"
 
 kill $(cat /var/run/mysqld/mysqld.pid)
 
